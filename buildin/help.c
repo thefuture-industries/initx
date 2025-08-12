94700e2461fcee6dfdbebb686986962ef44ecfa2
noneandundefined <aworkprogramm@gmail.com>
2025-08-12 14:24:30 -0400

    [refactor] Processed file - initx.c is refactored

---
+++ buildin/help.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int cmd_help(int argc, char **argv) 
+{
+    return 0;
+}
--- initx.c
+++ initx.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+struct cmd_struct 
 {
-    printf("hello, word\n");
+    const char *cmd;
+    int (*fn)(int, char **);
+};
+
+// static struct cmd_struct commands[]
+// {
+    
+// };
+
+int main(int argc, const char **argv)
+{
+    const char *cmd;
+    int done_help = 0;
+
+    cmd = argv[0];
+    if (!cmd) 
+        cmd = "initx-help";
+
+    return 0;
 }
