14d0c67314c014259132783de035b92b9cc01d5a
noneandundefined <aworkprogramm@gmail.com>
2025-08-13 17:41:56 -0400

    added base script for init

--- builtin/init.c
+++ builtin/init.c
@@ -1,9 +1,30 @@
 #include "builtin.h"
 
 #include <stdio.h>
+#include <string.h>
+
+const char *supported_languages[] = {"golang", "c"};
+const char *supported_scopes[] = {"backend"};
+
+int is_supported(const char *value, const char *list[]) {
+    for (size_t i = 0, n = sizeof(list)/sizeof(list[0]); i < n; i++) {
+        if (strcmp(value, list[i]) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 int cmd_init(int argc, const char **argv) 
 {
-    printf("this is init\n");
+    const char *language = argv[2];
+    const char *project_name = argv[3];
+
+    if (!is_supported(language, supported_languages)) {
+        fprintf(stderr, "error: unsupported language: '%s'\n", language);
+        return 1;
+    }
+    
     return 0;
 }
