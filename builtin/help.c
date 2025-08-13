b1c40683896dab048f49d23f52e7b5aba7bb1917
noneandundefined <aworkprogramm@gmail.com>
2025-08-13 17:22:02 -0400

    [fix] Updated builtin

--- builtin/help.c
+++ builtin/help.c
@@ -2,8 +2,19 @@
 
 #include <stdio.h>
 
+const char *initx_usage = 
+    "usage: initx [-v | --version] [-h | --help] [-l | --languages]\n"
+    "             [-s | --scopes] [--directory=<current | new>]\n"
+    "             <command> [<args>]\n";
+
+const char *initx_work_arch =
+    "working with architecture creation:\n"
+    "   initx [language] [project-name] [scope]";
+
 int cmd_help(int argc, const char **argv) 
 {
-    printf("this is help\n");
+    printf(initx_usage);
+    printf("\n%s", initx_work_arch);
+    
     return 0;
 }
